742aadb044f217833ec7578fee91a7e045131df8
PratikRangari <rangaripratik1998@gmail.com>
2024-02-13 19:11:18 +0530

    text colors

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { BiLogoTwitter, BiLogoFacebook, BiLogoLinkedin, BiLogoSkype, BiLogoInstagram,} from 'react-icons/bi';
+import team1 from '../assets/Images/team/team-1.jpg'
+import team2 from '../assets/Images/team/team-2.jpg'
+import team3 from '../assets/Images/team/team-3.jpg'
+import team4 from '../assets/Images/team/team-4.jpg'
+
 
 export default function Team() {
     const teamMembers = [
-        { name: "Walter White", position: "Chief Executive Officer", imageSrc: "assets/img/team/team-1.jpg" },
-        { name: "Sarah Jhonson", position: "Product Manager", imageSrc: "assets/img/team/team-2.jpg" },
-        { name: "William Anderson", position: "CTO", imageSrc: "assets/img/team/team-3.jpg" },
-        { name: "Amanda Jepson", position: "Accountant", imageSrc: "assets/img/team/team-4.jpg" }
+        { name: "Walter White", position: "Chief Executive Officer", imageSrc: team1 },
+        { name: "Sarah Jhonson", position: "Product Manager", imageSrc: team2 },
+        { name: "William Anderson", position: "CTO", imageSrc: team3 },
+        { name: "Amanda Jepson", position: "Accountant", imageSrc: team4 }
       ];
   return (
     <>
@@ -28,10 +34,10 @@ export default function Team() {
                   <h4>{member.name}</h4>
                   <span>{member.position}</span>
                   <div className="social">
-                    <Link to={member.socialtwitter}><i className="bi bi-twitter"></i></Link>
-                    <Link to=""><i className="bi bi-facebook"></i></Link>
-                    <Link to=""><i className="bi bi-instagram"></i></Link>
-                    <Link to=""><i className="bi bi-linkedin"></i></Link>
+                    <Link to={member.socialtwitter}><i className="bi bi-twitter"><BiLogoTwitter/></i></Link>
+                    <Link to={member.socialfacebook}><i className="bi bi-facebook"><BiLogoFacebook/></i></Link>
+                    <Link to={member.socialinstagram}><i className="bi bi-instagram"><BiLogoInstagram/></i></Link>
+                    <Link to={member.sociallinkedin}><i className="bi bi-linkedin"><BiLogoLinkedin/></i></Link>
                   </div>
                 </div>
               </div>
